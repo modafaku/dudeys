040311695aba3346b962670ce9a304a2c5908079
modafaku <modafaku@gmail.com>
2024-07-21 12:16:31 -0400

    matthiesenxyzghostcms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,36 @@
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import tailwind from "@astrojs/tailwind"
-import solidJs from "@astrojs/solid-js"
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+
+import matthiesenxyzghostcms from "@matthiesenxyz/astro-ghostcms";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://modafaku.com",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
-})
+  integrations: [
+    matthiesenxyzghostcms({
+      ghostURL: "https://ghost.modafaku.com",
+      ThemeProvider: {
+        disableThemeProvider: true,
+        theme: "@matthiesenxyz/astro-ghostcms-theme-default",
+      },
+      disableDefault404: true,
+      enableRSSFeed: true,
+      enableOGImages:true,
+      verbose: false,
+      Integrations: {
+        robotsTxt: {
+        },
+        sitemap: {
+        },
+      }
+    }),    
+    mdx(), 
+    sitemap(), 
+    solidJs(), 
+    tailwind({
+    applyBaseStyles: false
+  }), ]
+});
