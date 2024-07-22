ed6b431a8edb615b6a2bef2cd676493ab7472714
modafaku <modafaku@gmail.com>
2024-07-21 23:18:08 -0400

    dont even know

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,25 @@
+//import preload from "astro-preload";
 import { passthroughImageService, defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import matthiesenxyzghostcms from "@matthiesenxyz/astro-ghostcms";
-
 import cloudflare from "@astrojs/cloudflare";
+import linkPreview from "astro-link-preview";
+
+import satori from "satori-astro";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
-    service: passthroughImageService(),
+    service: passthroughImageService()
   },
   site: "https://modafaku.com",
-  integrations: [matthiesenxyzghostcms({
+  integrations: [
+    linkPreview(), 
+//    preload(), 
+    matthiesenxyzghostcms({
     ghostURL: "https://ghost.modafaku.com",
     ThemeProvider: {
       disableThemeProvider: true,
@@ -29,7 +35,7 @@ export default defineConfig({
     }
   }), mdx(), sitemap(), solidJs(), tailwind({
     applyBaseStyles: false
-  })],
-//  output: "server",
-//  adapter: cloudflare()
+  }), satori()]
+  //  output: "server",
+  //  adapter: cloudflare()
 });
